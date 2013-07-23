979bc9320247ffaa4969c19ea1ad10a7880dbe4a
Artyom Olshevskiy <siasiamail@gmail.com>
2013-07-24 02:49:24 +0300

    Adopt new configure make style and remove drops dir

--- build.sh
+++ build.sh
@@ -1,33 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+set -e
+cd $(dirname $0)/openjdk7
+unset JAVA_HOME
 
-## system-dependent local folders
-## path to installed Sun JDK 6:
-ALT_BOOTDIR=/opt/java6
+bash ./configure
+make all
 
-#########################################################################################
-#      DO NOT EDIT
-#########################################################################################
 
-#set -x
 
-## exporting local env vars:
-export ALT_BOOTDIR
-## resolving canonical path of current dir + '/drops' suffix:
-export ALT_DROPS_DIR="$( readlink -f "$( dirname "$0" )" )/openjdk7/drops"
-
-## unchangable exports:
-export LANG=C
-export JAVA_HOME=""
-export LD_LIBRARY_PATH=""
-export BUILD_NUMBER=b00
-export MILESTONE=fontfix
-export ALLOW_DOWNLOADS=false
-export DISABLE_HOTSPOT_OS_VERSION_CHECK=true
-NCPUS=4
-export HOTSPOT_BUILD_JOBS=$NCUPS
-export PARALLEL_COMPILE_JOBS=$NCPUS
-
-## starting real build process in subshell:
-(
-  cd $(dirname $0)/openjdk7 && make sanity && make
-)
--- clean.sh
+++ clean.sh
@@ -1,29 +1,3 @@
 #!/bin/sh
-
-## system-dependent local folders
-## path to installed Sun JDK 6:
-ALT_BOOTDIR=/opt/java6
-
-#########################################################################################
-#      DO NOT EDIT
-#########################################################################################
-
-#set -x
-
-## exporting local env vars:
-export ALT_BOOTDIR
-## resolving canonical path of current dir + '/drops' suffix:
-export ALT_DROPS_DIR="$( readlink -f "$( dirname "$0" )" )/openjdk7/drops"
-
-## unchangable exports:
-export LANG=C
-export JAVA_HOME=""
-export LD_LIBRARY_PATH=""
-export BUILD_NUMBER=b00
-export MILESTONE=fontfix
-export ALLOW_DOWNLOADS=false
-
-## starting real build process in subshell:
-(
-  cd $(dirname $0)/openjdk7 && make clean
-)
+cd $(dirname $0)/openjdk7
+make dist-clean
